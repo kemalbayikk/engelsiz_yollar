4ecfd2712f9fca5d60c0fd40d324161f71a3bf39
Msami <samil.demir@hakanbt.com.tr>
2021-04-17 15:09:38 +0300

    notifications

--- lib/view/home/anasayfa/view/ana_sayfa_view.dart
+++ lib/view/home/anasayfa/view/ana_sayfa_view.dart
@@ -1,14 +1,33 @@
 import 'package:engelsiz_yollar/core/extensions/context_extensions.dart';
 import 'package:engelsiz_yollar/core/extensions/num_extensions.dart';
+import 'package:engelsiz_yollar/view/home/anasayfa/viewmodel/ana_sayfa_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class AnasayfaView extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
- return Scaffold(
-body: Center(
-child: Text('AnasayfaView'),
-),
-);
-}
+  final AnasayfaViewModel _viewModel = AnasayfaViewModel();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Row(
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              _viewModel.increment();
+            },
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              _viewModel.decrement();
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Observer(builder: (_) {
+          return Text('${_viewModel.myNum}');
+        }),
+      ),
+    );
+  }
 }
